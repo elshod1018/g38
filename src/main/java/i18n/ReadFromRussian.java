d0d087819624c1b5a6aa11adc4675da2c929fdba
elshod <nuriddinovelshod2003@gmail.com>
2024-04-25 20:25:14 +0500

    changed

--- src/main/java/i18n/ReadFromRussian.java
+++ src/main/java/i18n/ReadFromRussian.java
@@ -1,11 +1,6 @@
 package i18n;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.Locale;
-import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
 public class ReadFromRussian {
